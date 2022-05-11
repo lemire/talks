d84dd0c63f8cc7c9d5f52ae989ea94421316be33
Daniel Lemire <lemire@gmail.com>
2022-05-11 14:59:36 -0400

    Minor cleaning.

--- 2022/evil/week6/db.py
+++ 2022/evil/week6/db.py
@@ -4,7 +4,6 @@ with open("secret.txt", "r") as file:
 
 import boto3
 dynamodb = boto3.resource('dynamodb', aws_access_key_id="AKIAWTSKHA76FSSB2Z7S", aws_secret_access_key=secret, region_name="ca-central-1" )
-#print(dynamodb.list_tables()["TableNames"])
 tables = list(dynamodb.tables.all())
 print("tables=",tables)
 
@@ -13,36 +12,14 @@ print(evil)
 
 response = evil.scan()
 print(response)
-#import json
-#response = json.loads(evil.scan())
-#print(response)
-#data = response['Items']
 
-#while 'LastEvaluatedKey' in response:
-#    response = table.scan(ExclusiveStartKey=response['LastEvaluatedKey'])
-#    data.extend(response['Items'])
 lat = 45.54187056861444
 long = -73.48202822340973
-#import struct
 
-# define double_to_hex (or float_to_hex)
-#def double_to_hex(f):
-#    return hex(struct.unpack('<Q', struct.pack('<d', f))[0])
-#print(double_to_hex(lat))
-#print(double_to_hex(long))
-
-#key = double_to_hex(lat)+double_to_hex(long)
 import uuid
-
 myuuid = uuid.uuid4()
-from decimal import Decimal
 
-#response = evil.put_item(
-#    Item={
-#        'latitude': str(lat),
-#        'longitude': str(long),
-#    }
-#)
+
 print(response)
 response = evil.put_item(
     Item={
